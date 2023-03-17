6101b41aaaf40ca1d1ba3388122c06cfcd688252
avishak <aurgho1998@gmail.com>
2023-03-18 00:15:06 +0530

    Flow emitting error fix.

--- app/src/main/java/com/clonedsemicolon/dalle2_android/ui/viewmodel/GenerateImageViewModel.kt
+++ app/src/main/java/com/clonedsemicolon/dalle2_android/ui/viewmodel/GenerateImageViewModel.kt
@@ -1,27 +1,31 @@
 package com.clonedsemicolon.dalle2_android.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.load.engine.Resource
+import com.clonedsemicolon.dalle2_android.common.ImageProgressStatus
 import com.clonedsemicolon.dalle2_android.data.model.GeneratedImage
 import com.clonedsemicolon.dalle2_android.data.model.RequestModel
 import com.clonedsemicolon.dalle2_android.domain.usecase.GenerateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GenerateImageViewModel @Inject constructor(private val generateUseCase:GenerateUseCase) : ViewModel() {
+class GenerateImageViewModel @Inject constructor(private val generateUseCase:GenerateUseCase) :
+    ViewModel() {
 
-    private val _state = MutableStateFlow<Resource<GeneratedImage>?>(null)
+    private val _state = MutableStateFlow<ImageProgressStatus<GeneratedImage>?>(null)
     val state = _state.asStateFlow()
 
-
-    fun generateImageFromPrompt(requestModel: RequestModel){
-        generateUseCase(requestModel).collect{
-            _state.emit(it)
+    fun generateImageFromPrompt(requestModel: RequestModel) {
+        viewModelScope.launch {
+            generateUseCase(requestModel).collect {
+                _state.emit(it)
+            }
         }
-
     }
 }
 
